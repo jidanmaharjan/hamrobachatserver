ca187745c32d7ea174509896497989e34a5338e3
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-10-21 22:07:46 +0545

    get route change

--- backend/routes/overall.js
+++ backend/routes/overall.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const { changeOverall, getOverall } = require("../controllers/overallController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
-router.route('/getoverall').put(isAuthenticatedUser,getOverall)
+router.route('/getoverall').get(isAuthenticatedUser,getOverall)
 router.route('/changeoverall').put(isAuthenticatedUser,authorizeRoles('admin'),changeOverall)
 
 module.exports = router
