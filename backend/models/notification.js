2300fba7d5863e92f3c9f029d80d49a2f8629f5e
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-11-30 21:34:02 +0545

    expire time

--- backend/models/notification.js
+++ backend/models/notification.js
@@ -1,31 +1,33 @@
-const  mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 const moment = require("moment");
 
 const notificationSchema = new mongoose.Schema({
-    date: {
-        type: String,
+  date: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  seen: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
         required: true,
+      },
     },
-    title: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    seen: [{
-        user:{
-            type: mongoose.Schema.ObjectId,
-            ref: 'User',
-            required: true,
-        }
+  ],
+  expireAt: {
+    type: Date,
+    default: Date.now(),
+    index: { expireAfterSeconds: 604800 },
+  },
+});
 
-    }],
-    expireAt: {
-        type: Date,
-        default: Date.now()+30*24*60*60
-    }
-})
-
-module.exports = mongoose.model('Notification',notificationSchema)
+module.exports = mongoose.model("Notification", notificationSchema);
